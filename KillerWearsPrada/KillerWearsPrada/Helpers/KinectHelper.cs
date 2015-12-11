c6cf449e2ac3c5950198a5a13492cabbd3b72bea
Monica Cecere <cecere.monica@gmail.com>
2015-12-11 13:02:36 +0100

    Aggiunta classi helper kinect  QRreader e classe che maneggia da dove prendere l'immagine con qr/barcode da leggere
    Monica Cecere

---
+++ KillerWearsPrada/KillerWearsPrada/Helpers/KinectHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KillerWearsPrada.Helpers
+{
+    class KinectHelper
+    {
+
+        /// <summary>
+        /// Ristorna un valore che indica se il kinect è collegato
+        /// </summary>
+        public static Boolean IsKinectConected
+        {
+            //appena possibile implementare il codice per capire se il kinect é collegato
+            get { return false; }
+
+        }
+    }
+    
+}
