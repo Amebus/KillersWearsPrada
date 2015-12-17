a49e987022e5711796c2fcec5799979edfde106d
Caielli <andrea.caielli@mail.polimi.it>
2015-12-17 11:23:16 +0100

    configurazione del DBHelper per accettare queries

--- KillerWearsPrada/KillerWearsPrada/Helpers/DBHelper.cs
+++ KillerWearsPrada/KillerWearsPrada/Helpers/DBHelper.cs
@@ -40,8 +40,68 @@ namespace KillerWearsPrada.Helpers
             }
 
             DBConection.Close();
-
             return "Connessione avvenuta con successo";
         }
     }
+
+    class Queries
+    {
+        public const String q1 = "query";
+    }
+
+    /* parametrized query v1
+    using (AdventureWorksEntities context =
+    new AdventureWorksEntities())
+    {
+    // Create a query that takes two parameters.
+    string queryString =
+        @"SELECT VALUE Contact FROM AdventureWorksEntities.Contacts 
+                AS Contact WHERE Contact.LastName = @ln AND
+                Contact.FirstName = @fn";
+
+    ObjectQuery<Contact> contactQuery =
+        new ObjectQuery<Contact>(queryString, context);
+
+    // Add parameters to the collection.
+    contactQuery.Parameters.Add(new ObjectParameter("ln", "Adams"));
+    contactQuery.Parameters.Add(new ObjectParameter("fn", "Frances"));
+
+    // Iterate through the collection of Contact items.
+    foreach (Contact result in contactQuery)
+        Console.WriteLine("Last Name: {0}; First Name: {1}",
+        result.LastName, result.FirstName);
+    }
+    */
+    /*parametrized query v2
+    private static void UpdateDemographics(Int32 customerID,
+    string demoXml, string connectionString)
+    {
+    // Update the demographics for a store, which is stored 
+    // in an xml column. 
+    string commandText = "UPDATE Sales.Store SET Demographics = @demographics "
+        + "WHERE CustomerID = @ID;";
+
+    using (SqlConnection connection = new SqlConnection(connectionString))
+    {
+        SqlCommand command = new SqlCommand(commandText, connection);
+        command.Parameters.Add("@ID", SqlDbType.Int);
+        command.Parameters["@ID"].Value = customerID;
+
+        // Use AddWithValue to assign Demographics.
+        // SQL Server will implicitly convert strings into XML.
+        command.Parameters.AddWithValue("@demographics", demoXml);
+
+        try
+        {
+            connection.Open();
+            Int32 rowsAffected = command.ExecuteNonQuery();
+            Console.WriteLine("RowsAffected: {0}", rowsAffected);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+    }
+    */
 }
