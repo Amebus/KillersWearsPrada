f51cb733a0683d11d16e7fa5fcc7b3ef473a2c08
Caielli <andrea.caielli@mail.polimi.it>
2016-01-15 16:45:30 +0100

    aggiunte in DB e modifica queries

--- KillerWearsPrada/KillerWearsPrada/Helpers/DBHelper.cs
+++ KillerWearsPrada/KillerWearsPrada/Helpers/DBHelper.cs
@@ -59,39 +59,59 @@ namespace KillerWearsPrada.Helpers
         ///         7. Texture file name
         ///         8. Mask file name
         /// </returns>
-        public Item GetItemFromClues(E_Shape long1, E_Gradiation light, E_Texture texture, E_ItemKind itemKind){
+        public Item GetItemFromClues(E_Shape long1, E_Gradiation light, E_Texture textureKind, E_ItemKind itemKind){
 
             DBConnection.Open();
             // note : we want items of which we have more than 10 available 
-            string query = "SELECT TOP 1 C.ID,D.Barcode, D.Nome, D.Prezzo, D.Descrizione, D.Reparto, T.FileName, M.FileName, D.Immagine FROM" 
-                + " Capo AS C,DatiNegozio AS D, Grafica AS G,Texture AS T, Maschera AS M, TipoCapo AS TC"
-                + " WHERE C.DatiNegozio = D.ID AND C.Grafica = G.ID AND G.Texture = T.ID AND G.Maschera = M.ID AND TC.ID = D.Tipo" 
-                +" AND D.Disponibilità > 10 AND D.Lungo=@p1 AND T.Chiaro=@p2 AND T.TipoTexture = @p3 AND TC.Tipo = @p4 ORDER BY rnd(C.ID); ";
-            //string query = "SELECT * FROM Capo";
-            OleDbCommand command = new OleDbCommand(query, DBConnection);
-            // add parameters
-            // long parameter - @p1  
-            Boolean shape = false;
-            if (long1 == E_Shape.LUNGO)
-                shape = true; 
-                    
-            command.Parameters.Add("@p1", OleDbType.Boolean).Value = shape;
-            // dark parameter - @p2
-            Boolean grad = false;
-            if (light == E_Gradiation.CHIARO)
-                grad = true;
+            string query = "SELECT TOP 1 C.ID,D.Barcode, D.Nome, D.Prezzo, D.Descrizione, D.Reparto, T.FileName, M.FileName, D.Immagine";
+            query += " FROM Capo AS C,DatiNegozio AS D, Grafica AS G,Texture AS T, Maschera AS M, TipoCapo AS TC, TipoTexture AS TT";
+            query += " WHERE C.DatiNegozio = D.ID AND C.Grafica = G.ID AND G.Texture = T.ID AND G.Maschera = M.ID AND TC.ID=D.Tipo";
+            query += " AND T.TipoTexture = TT.ID D.Disponibili > 10 AND D.Lungo=@p1 AND T.Chiaro=@p2 AND  TT.Nome = @p3 AND TC.Tipo = @p4 ORDER BY rnd(C.ID)";
 
-            command.Parameters.Add("@p2", OleDbType.Boolean).Value = grad;
-            // texture parameter - @p3
-            command.Parameters.Add("@p3", OleDbType.VarChar, 255).Value = texture.ToString();
-            // item kind parameter -@p4
-            command.Parameters.Add("@p4", OleDbType.VarChar, 255).Value = itemKind.ToString();
+            // parameter @p1 - shape
+            if (long1 == E_Shape.LUNGO)
+            {
+                query = query.Replace("@p1", true.ToString());
+            }
+            else
+            {
+                query = query.Replace("@p1", false.ToString());
+            }
+
+            // parameter @p2 - gradiation
+            if (light == E_Gradiation.CHIARO)
+            {
+                query = query.Replace("@p2", true.ToString());
+            }
+            else
+            {
+                query = query.Replace("@p2", false.ToString());
+            }
+
+            // parameter @p3 - texture kind 
+            query = query.Replace("@p3", "\'" + textureKind.ToString() + "\'");
+
+            // parameter @p4 - item kind 
+            query = query.Replace("@p4", "\'" + itemKind.ToString() + "\'");
+
+            OleDbCommand command = new OleDbCommand(query, DBConnection);
 
             OleDbDataReader result = command.ExecuteReader();
+
             result.Read();
-            Item i = new Item((int)result.GetValue(0),result.GetValue(1).ToString(), result.GetValue(2).ToString(), 
-                (float)result.GetValue(3), result.GetValue(4).ToString(), result.GetValue(5).ToString(), 
-                result.GetValue(6).ToString(), result.GetValue(7).ToString(),result.GetValue(8).ToString());
+
+            int codice = result.GetInt32(0);
+            String barcode = result.GetString(1);
+            String name = result.GetString(2);
+            Double price = result.GetDouble(3);
+            String descr = result.GetString(4);
+            String rep = result.GetString(5);
+            String texture = result.GetString(6);
+            String mask = result.GetString(7);
+            String image = result.GetString(8);
+            //throw new NotImplementedException("sistemare queries");
+
+            Item i = new Item(codice, barcode, name, price, descr, rep, texture, mask, image);
 
             DBConnection.Close();
 
@@ -119,52 +139,42 @@ namespace KillerWearsPrada.Helpers
         {
 
             DBConnection.Open();
+            
             // note : we want items of which we have more than 10 available 
-            //string query = "SELECT TOP 1 C.ID,D.Barcode, D.Nome, D.Prezzo, D.Descrizione, D.Reparto, T.FileName, M.FileName, D.Immagine FROM Capo AS C,DatiNegozio AS D, Grafica AS G,Texture AS T, Maschera AS M, TipoCapo AS TC   WHERE C.DatiNegozio = D.ID AND C.Grafica = G.ID AND G.Texture = T.ID AND G.Maschera = M.ID AND TC.ID = C.Tipo AND D.Disponibilità > 10 AND (((D.Lungo/Corto)=@p1)) AND TC.Tipo = @p4 ORDER BY rnd(C.ID); ";
+            
             string query = "SELECT TOP 1 C.ID,D.Barcode, D.Nome, D.Prezzo, D.Descrizione, D.Reparto, T.FileName, M.FileName, D.Immagine";
             query += " FROM Capo AS C,DatiNegozio AS D, Grafica AS G,Texture AS T, Maschera AS M, TipoCapo AS TC";
             query += " WHERE C.DatiNegozio = D.ID AND C.Grafica = G.ID AND G.Texture = T.ID AND G.Maschera = M.ID AND TC.ID=D.Tipo";
             query += " AND D.Disponibili > 10 AND D.Lungo=@p1 AND TC.Tipo = @p4 ORDER BY rnd(C.ID)";
-            //query += " WHERE C.DatiNegozio = D.ID AND C.Grafica = G.ID AND G.Texture = T.ID AND G.Maschera = M.ID AND TC.ID = C.Tipo AND D.Disponibili > 10";// AND (((D.Lungo/Corto)=@p1)) AND TC.Tipo = @p4 ORDER BY rnd(C.ID); ";
 
-
-            //string query = "SELECT * FROM Capo";
-            // add parameters
-            // long parameter - @p1    
-            // shape = "no";
+            // parameter @p1 - shape
             if (long1 == E_Shape.LUNGO)
             {
                 query = query.Replace("@p1", true.ToString());
             }
-                //shape = "yes";    
-                  
             else
+            {
                 query = query.Replace("@p1", false.ToString());
-            //command.Parameters.Add("@p1", OleDbType.VarChar).Value = shape;
-
-            // item kind parameter -@p4
-            //command.Parameters.Add("@p4", OleDbType.VarChar, 255).Value = itemKind.ToString();
-
+            }
+                
+            // parameter @p4 - item kind 
             query = query.Replace("@p4", "\'" + itemKind.ToString() + "\'");
             
             OleDbCommand command = new OleDbCommand(query, DBConnection);
 
             OleDbDataReader result = command.ExecuteReader();
             result.Read();
-            //int wvColumnCodice = result.GetOrdinal("C.ID");
 
-            //result.GetValue()
             int codice = result.GetInt32(0);
             String barcode = result.GetString(1);
             String name = result.GetString(2);
             Double price = result.GetDouble(3);
-            String descr = "";//result.GetString(4);
+            String descr = result.GetString(4);
             String rep = result.GetString(5);
             String texture = result.GetString(6);
             String mask = result.GetString(7);
             String image = result.GetString(8);
-
-            throw new NotImplementedException("sistemare queries");
+            //throw new NotImplementedException("sistemare queries");
 
             Item i = new Item(codice, barcode, name, price , descr, rep, texture , mask, image);
 
@@ -189,30 +199,49 @@ namespace KillerWearsPrada.Helpers
         ///         6. Item Reparto
         ///         7. Texture file name
         ///         8. Mask file name
+        ///         9. Image file name
         /// </returns>
         public Item GetItemByGradation(E_Gradiation light, E_ItemKind itemKind)
         {
 
             DBConnection.Open();
             // note : we want items of which we have more than 10 available 
-            string query = "SELECT TOP 1 C.ID,D.Barcode, D.Nome, D.Prezzo, D.Descrizione, D.Reparto, T.FileName, M.FileName, D.Immagine FROM Capo AS C,DatiNegozio AS D, Grafica AS G,Texture AS T, Maschera AS M, TipoCapo AS TC   WHERE C.DatiNegozio = D.ID AND C.Grafica = G.ID AND G.Texture = T.ID AND G.Maschera = M.ID AND TC.ID = C.Tipo AND D.Disponibilità > 10 AND (((T.[Chiaro/Scuro])=@p2)) AND TC.Tipo = @p4 ORDER BY rnd(C.ID); ";
-            //string query = "SELECT * FROM Capo";
-            OleDbCommand command = new OleDbCommand(query, DBConnection);
-            // add parameters
-            // dark parameter - @p2
-            Boolean grad = false;
+            string query = "SELECT TOP 1 C.ID,D.Barcode, D.Nome, D.Prezzo, D.Descrizione, D.Reparto, T.FileName, M.FileName, D.Immagine";
+            query += " FROM Capo AS C,DatiNegozio AS D, Grafica AS G,Texture AS T, Maschera AS M, TipoCapo AS TC";
+            query += " WHERE C.DatiNegozio = D.ID AND C.Grafica = G.ID AND G.Texture = T.ID AND G.Maschera = M.ID AND TC.ID=D.Tipo";
+            query += " AND D.Disponibili > 10 AND T.Chiaro=@p2 AND TC.Tipo = @p4 ORDER BY rnd(C.ID)";
+
+            // parameter @p2 - gradiation
             if (light == E_Gradiation.CHIARO)
-                grad = true;
-            command.Parameters.Add("@p2", OleDbType.Boolean).Value = grad;
-            
-            // item kind parameter -@p4
-            command.Parameters.Add("@p4", OleDbType.VarChar, 255).Value = itemKind.ToString();
+            {
+                query = query.Replace("@p2", true.ToString());
+            }
+            else
+            {
+                query = query.Replace("@p2", false.ToString());
+            }
+
+            // parameter @p4 - item kind 
+            query = query.Replace("@p4", "\'" + itemKind.ToString() + "\'");
+
+            OleDbCommand command = new OleDbCommand(query, DBConnection);
 
             OleDbDataReader result = command.ExecuteReader();
 
-            Item i = new Item((int)result.GetValue(0), result.GetValue(1).ToString(), result.GetValue(2).ToString(),
-                (float)result.GetValue(3), result.GetValue(4).ToString(), result.GetValue(5).ToString(),
-                result.GetValue(6).ToString(), result.GetValue(7).ToString(), result.GetValue(8).ToString());
+            result.Read();
+
+            int codice = result.GetInt32(0);
+            String barcode = result.GetString(1);
+            String name = result.GetString(2);
+            Double price = result.GetDouble(3);
+            String descr = result.GetString(4);
+            String rep = result.GetString(5);
+            String texture = result.GetString(6);
+            String mask = result.GetString(7);
+            String image = result.GetString(8);
+            //throw new NotImplementedException("sistemare queries");
+
+            Item i = new Item(codice, barcode, name, price, descr, rep, texture, mask, image);
 
             DBConnection.Close();
 
@@ -229,6 +258,8 @@ namespace KillerWearsPrada.Helpers
             DBConnection.Open();
             
             string query = "UPDATE Utente SET Punteggio = @value WHERE ID = @id;";
+
+            
             OleDbCommand command = new OleDbCommand(query, DBConnection);
             command.Parameters.Add("@value", OleDbType.Integer).Value = score;
             command.Parameters.Add("@id", OleDbType.Integer).Value = player;
