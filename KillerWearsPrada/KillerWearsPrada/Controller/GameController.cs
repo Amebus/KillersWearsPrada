142bdb05909a42110035c94139c30c44991f4637
Federico Maria Ceruti <10400685@polimi.it>
2016-01-16 11:48:09 +0100

    altre modifiche per la creazione del gioco

--- KillerWearsPrada/KillerWearsPrada/Controller/GameController.cs
+++ KillerWearsPrada/KillerWearsPrada/Controller/GameController.cs
@@ -138,12 +138,121 @@ namespace KillerWearsPrada.Controller
 
             wvID = wvID.Replace(' ', '-');
             wvID += ("-" + PlayerName);
-            Model.Game wvGame = new Model.Game(wvID, PlayerName, NUMBER_OF_ROOMS, ITEMS_PER_ROOM);
+            Model.Game wvGame = new Model.Game(wvID, PlayerName);
             
             wvPath = System.IO.Path.Combine(wvPath, wvID);
             Helpers.SerializerHelper.Serialize(wvPath, wvGame);
 
             throw new NotImplementedException("Implementare qui la stampa dei QRCODE");
+        }
+
+        /// <summary>
+        /// Generate a list of <see cref="Item,"/> to be put in a <see cref="Room"/>
+        /// </summary>
+        /// <param name="NumberOfItems"></param>
+        /// <returns></returns>
+        private void CreateGame(Int32 NumberOfItems)
+        {
+            Model.Solution wvSolution = new Model.Solution();
+            List<Model.Item> wvItems = new List<Model.Item>();
+            //Random wvRND = new Random(NumberOfItems);
+            /*
+            per prima cosa estraggo le caratteristiche che userò per la ricerca del capo
+            sicuramente avremo come indizi shape e gradation (sono  facili da gestire)
+                    ma non ha senso per i cappelli, per quelli ci vuole per forza colore + texture 
+            randGradiation = .. 
+            randPositive1 = .. 
+            randShape = ..
+            randPositive2 = ..
+            rand1 = .. <-- decide se estrarre come risolutivo Texture o Color
+            rand2 = .. <-- estare a caso da Texture o Color
+            randPositive3 = ..
+           
+
+             qui estraiamo i valori casuali e li salviamo
+             ...
+             in pratica vorrei che si potesse salvare in delle variabli l'E_xyz.random corretto per ogni caso
+             ....
+             
+            clue1= E_Gradiation(rand)
+            clue2= E_Shape
+            clue3=...
+
+            Boolean positive1 = true;
+            if(!randPositive1%3)
+            {
+                positive1 = false;
+            }
+            Boolean positive2 = true;
+            if(!randPositive2%3)
+            {
+                positive2 = false;
+            }
+            Boolean positive3 = true;
+            if(!randPositive3%3)
+            {
+                positive3 = false;
+            }
+            
+            //aggiungiamo le nuove clues alla stanza
+            al posto di E_xyz sostituiamo il valore rand corretto ed il resto va a NULL
+            //la prima clue è per gradation
+            Clue c1 = new Clue(positive1, E_Gradiation.CHIARO, E_Shape.NULL, E_Color.NULL, E_Texture.NULL); ;
+            this.ActualRoom.AddClue(c1);
+            // la seconda è per shape 
+            Clue c2 = new Clue(positive2, NULL, E_Shape.LUNGO, E_Color.NULL, E_Texture.NULL); ;
+            this.ActualRoom.AddClue(c2);
+            // la terza dipende da rand1
+            Clue c3 = new Clue(positive3, E_Gradiation.NULL, E_Shape.NULL, E_Color.x, E_Texture.y); ;
+            this.ActualRoom.AddClue(c3);
+
+            1° capo -quello giusto - corrisponde a 3 indizi - gradation + shape + 3°
+            wvItems.Add(wvDB.GetItemFromClues(clue1,clue2,NULL,clue3,ItemKind));
+            // aggiungi la clue all'item 1
+            2° capo - corrisponde a 2 indizi gradation + shape
+            wvItems.Add(wvDB.GetItemFromClues(clue1,clue2,NULL,!clue3,ItemKind));
+            // aggiungi la clue all'item 2
+            3° e 4° capo  - solo shape
+            wvItems.Add(wvDB.GetItemFromClues(clue1,!clue2,NULL,!clue3,ItemKind));
+            // aggiungi la clue all'item 3
+            wvItems.Add(wvDB.GetItemFromClues(clue1,!clue2,NULL,!clue3,ItemKind));
+            // aggiungi la clue all'item 4
+            5° e 6° capo - solo gradiation
+            wvItems.Add(wvDB.GetItemFromClues(!clue1,clue2,NULL,!clue3,ItemKind));
+            // aggiungi la clue all'item 5
+            wvItems.Add(wvDB.GetItemFromClues(!clue2,clue2,NULL,!clue3,ItemKind));
+            // aggiungi la clue all'item 6
+
+
+
+            ora a parte la traduzione dei metodi e variabili finti in qualcosa di più reale
+            doremmo avere una stanza popolata, e clues nelle stanze
+
+            */
+
+
+
+            if (!wvSolution.CheckItemKind(Model.E_ItemKind.Cappello))
+            {
+                wvSolution.AddItemKind(Model.E_ItemKind.Cappello);
+            }
+            else if (!wvSolution.CheckItemKind(Model.E_ItemKind.Maglietta))
+            {
+                wvSolution.AddItemKind(Model.E_ItemKind.Maglietta);
+            }
+            else if (!wvSolution.CheckItemKind(Model.E_ItemKind.Pantaloni))
+            {
+                wvSolution.AddItemKind(Model.E_ItemKind.Pantaloni);
+            }
+
+
+            //Il primo è quello giusto
+            wvItems.Add(attDataBase.GetItemByGradation(Model.E_Gradiation.CHIARO, wvSolution.LastItemKind));
+
+            NumberOfItems--;
+            //mi faccio dare gli altri (per ora sola il secondo)
+            wvItems.Add(attDataBase.GetItemByGradation(Model.E_Gradiation.SCURO, wvSolution.LastItemKind));
+            
         }
 
         public static void DeleteAllGames()
--- KillerWearsPrada/KillerWearsPrada/Model/Game.cs
+++ KillerWearsPrada/KillerWearsPrada/Model/Game.cs
@@ -31,28 +31,14 @@ namespace KillerWearsPrada.Model
         /// </summary>
         /// <param name="ID">ID of the Game</param>
         /// <param name="PlayerName">Name of the player</param>
-        /// <param name="Rooms">Number of Rooms in the game</param>
-        /// <param name="ItemsPerRoom">Number of items in each room</param>
-        public Game (String ID, String PlayerName, Int32 Rooms, Int32 ItemsPerRoom)
+        public Game (String ID, String PlayerName)
         {
             attStarted = false;
             attSolution = new Solution();
             attPlayer = new Player(ID, PlayerName);
             attScore = 0;
             attActualRoom = 0;
-
-            attRooms = new List<Room>(Rooms);
-
-            //creo singole stanze
-            attRooms[0] = null;
-
-            for(int i = 1; i < Rooms; i++)
-            {
-                attRooms[i] = new Room(CreateItems(ItemsPerRoom));
-            }
-
-            // una volta finite le stanze scambiare il 3° indizio che sarà dato in un'altra stanza
-
+            
 
         }
 
@@ -138,14 +124,25 @@ namespace KillerWearsPrada.Model
         public List<Room> Rooms
         {
             get { return attRooms; }
+            set { attRooms = value; }
         }
+        
+        public Solution Solution
+        {
+            get { return Solution; }
+            set { Solution = value; }
+        }
+
+        #endregion
+
+        #region Methods
 
         public List<Item> GetRoomItems(Int32 RoomIndex)
         {
             return attRooms[RoomIndex].Items;
         }
 
-        public Item GetItemByCode (Int32 Room, Int32 ItemCode)
+        public Item GetItemByCode(Int32 Room, Int32 ItemCode)
         {
             return attRooms[Room].GetItemByCode(ItemCode);
         }
@@ -154,125 +151,13 @@ namespace KillerWearsPrada.Model
         {
             return attRooms[Room].GetItemByCode(ItemBarCode);
         }
-        #endregion
 
-        #region Methods
         /// <summary>
         /// Compute player's score
         /// </summary>
         public void ComputeScore()
         {
 
-        }
-
-        /// <summary>
-        /// Generate a list of <see cref="Item,"/> to be put in a <see cref="Room"/>
-        /// </summary>
-        /// <param name="NumberOfItems"></param>
-        /// <returns></returns>
-        private List<Item> CreateItems(Int32 NumberOfItems)
-        {
-            Helpers.DBHelper wvDB = new Helpers.DBHelper();
-            List<Item> wvItems = new List<Item>();
-            //Random wvRND = new Random(NumberOfItems);
-            /*
-            per prima cosa estraggo le caratteristiche che userò per la ricerca del capo
-            sicuramente avremo come indizi shape e gradation (sono  facili da gestire)
-                    ma non ha senso per i cappelli, per quelli ci vuole per forza colore + texture 
-            randGradiation = .. 
-            randPositive1 = .. 
-            randShape = ..
-            randPositive2 = ..
-            rand1 = .. <-- decide se estrarre come risolutivo Texture o Color
-            rand2 = .. <-- estare a caso da Texture o Color
-            randPositive3 = ..
-           
-
-             qui estraiamo i valori casuali e li salviamo
-             ...
-             in pratica vorrei che si potesse salvare in delle variabli l'E_xyz.random corretto per ogni caso
-             ....
-             
-            clue1= E_Gradiation(rand)
-            clue2= E_Shape
-            clue3=...
-
-            Boolean positive1 = true;
-            if(!randPositive1%3)
-            {
-                positive1 = false;
-            }
-            Boolean positive2 = true;
-            if(!randPositive2%3)
-            {
-                positive2 = false;
-            }
-            Boolean positive3 = true;
-            if(!randPositive3%3)
-            {
-                positive3 = false;
-            }
-            
-            //aggiungiamo le nuove clues alla stanza
-            al posto di E_xyz sostituiamo il valore rand corretto ed il resto va a NULL
-            //la prima clue è per gradation
-            Clue c1 = new Clue(positive1, E_Gradiation.CHIARO, E_Shape.NULL, E_Color.NULL, E_Texture.NULL); ;
-            this.ActualRoom.AddClue(c1);
-            // la seconda è per shape 
-            Clue c2 = new Clue(positive2, NULL, E_Shape.LUNGO, E_Color.NULL, E_Texture.NULL); ;
-            this.ActualRoom.AddClue(c2);
-            // la terza dipende da rand1
-            Clue c3 = new Clue(positive3, E_Gradiation.NULL, E_Shape.NULL, E_Color.x, E_Texture.y); ;
-            this.ActualRoom.AddClue(c3);
-
-            1° capo -quello giusto - corrisponde a 3 indizi - gradation + shape + 3°
-            wvItems.Add(wvDB.GetItemFromClues(clue1,clue2,NULL,clue3,ItemKind));
-            // aggiungi la clue all'item 1
-            2° capo - corrisponde a 2 indizi gradation + shape
-            wvItems.Add(wvDB.GetItemFromClues(clue1,clue2,NULL,!clue3,ItemKind));
-            // aggiungi la clue all'item 2
-            3° e 4° capo  - solo shape
-            wvItems.Add(wvDB.GetItemFromClues(clue1,!clue2,NULL,!clue3,ItemKind));
-            // aggiungi la clue all'item 3
-            wvItems.Add(wvDB.GetItemFromClues(clue1,!clue2,NULL,!clue3,ItemKind));
-            // aggiungi la clue all'item 4
-            5° e 6° capo - solo gradiation
-            wvItems.Add(wvDB.GetItemFromClues(!clue1,clue2,NULL,!clue3,ItemKind));
-            // aggiungi la clue all'item 5
-            wvItems.Add(wvDB.GetItemFromClues(!clue2,clue2,NULL,!clue3,ItemKind));
-            // aggiungi la clue all'item 6
-
-
-
-            ora a parte la traduzione dei metodi e variabili finti in qualcosa di più reale
-            doremmo avere una stanza popolata, e clues nelle stanze
-
-            */
-
-
-
-            if (!attSolution.CheckItemKind(E_ItemKind.Cappello))
-            {
-                attSolution.AddItemKind(E_ItemKind.Cappello);
-            }
-            else if(!attSolution.CheckItemKind(E_ItemKind.Maglietta))
-            {
-                attSolution.AddItemKind(E_ItemKind.Maglietta);
-            }
-            else if (!attSolution.CheckItemKind(E_ItemKind.Pantaloni))
-            {
-                attSolution.AddItemKind(E_ItemKind.Pantaloni);
-            }
-
-
-            //Il primo è quello giusto
-            wvItems.Add(wvDB.GetItemByGradation(E_Gradiation.CHIARO, attSolution.LastItemKind));
-           
-            NumberOfItems--;
-            //mi faccio dare gli altri (per ora sola il secondo)
-            wvItems.Add(wvDB.GetItemByGradation(E_Gradiation.SCURO, attSolution.LastItemKind));
-            
-            return wvItems;
         }
 
         #endregion
