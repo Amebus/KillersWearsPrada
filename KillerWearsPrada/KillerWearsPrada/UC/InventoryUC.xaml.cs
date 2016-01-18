91b8245c71bd61191fd736be3a99fda83a3208e2
moncec <cecere.monica@gmail.com>
2016-01-18 16:50:49 +0100

    modifiche inventario

--- KillerWearsPrada/KillerWearsPrada/UC/InventoryUC.xaml.cs
+++ KillerWearsPrada/KillerWearsPrada/UC/InventoryUC.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using KillerWearsPrada.Controller;
 using KillerWearsPrada.Model;
+using Microsoft.Kinect.Toolkit.Input;
+using Microsoft.Kinect.Wpf.Controls;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,7 +26,13 @@ namespace KillerWearsPrada.UC
     /// </summary>
     public partial class InventoryUC : UserControl
     {
-       
+
+        ObservableCollection<ItemProva> p;
+
+        ObservableCollection<ItemProva> SelectedItems = new ObservableCollection<ItemProva>();
+        ObservableCollection<ItemProva> SelectedItems2 = new ObservableCollection<ItemProva>();
+
+
         public InventoryUC()
         {
             MainWindow i = (MainWindow)Application.Current.Windows[0];
@@ -31,46 +41,56 @@ namespace KillerWearsPrada.UC
             InitializeComponent();
 
 
-            //TODO sarebbe così
-        /*
-            lvDataBinding.ItemsSource = i.attGameController.ItemsInInventory;
 
-            ListView1.ItemsSource = i.attGameController.ItemsInInventory;
-            */
+            //TODO sarebbe così
+            /*
+                lvDataBinding.ItemsSource = i.attGameController.ItemsInInventory;
+
+                ListView1.ItemsSource = i.attGameController.ItemsInInventory;
+                */
 
             //        List<Item> items = i.attGameController.ActualRoomItems;
-            List<Item> s = new List<Item>();
+            //       List<Item> s = new List<Item>();
 
-           // devo anche controllare che quelli nell'inventario non siano anche nel cestino???
-
-                //s.Add(new Item (2, "bc", "name",9.04,"descr", "rep","texture", @"C:\Users\Monica\Documents\polimi\AUI\project\repogithub\KillerWearsPrada\KillerWearsPrada\Images\textures\blueTartan.jpg", "kind" ));
-                //s.Add(new Item(2, "bc", "name2", 9.04, "descr", "rep", "texture", @"C:\Users\Monica\Documents\polimi\AUI\project\repogithub\KillerWearsPrada\KillerWearsPrada\Images\hat_1meleOk.png", "kind"));
-                //s.Add(new Item(2, "bc", "name3", 9.04, "descr", "rep", "texture", @"C:\Users\Monica\Documents\polimi\AUI\project\repogithub\KillerWearsPrada\KillerWearsPrada\Images\trashEmpty.png", "kind"));
-            
-      /*      items.Add(new ItemProva() { code = 345, description = "eee333", itemName = "ciaone", price = 3.5 });
-            items.Add(new ItemProva() { description = "Jane Doe", code = 39, imageFileName = "jane@doe-family.com", price = 0.888 });
-            items.Add(new ItemProva() { maskFileName = "Sammy Doe", price = 13, reparto = "sammy.doe@gmail.com", code = 1234, description = "capo brutto brutto" });
-            */
+            // devo anche controllare che quelli nell'inventario non siano anche nel cestino???
+            /*
+                            s.Add(new Item (2, "bc", "name",9.04,"descr", "rep","texture", @"C:\Users\Monica\Documents\polimi\AUI\project\repogithub\KillerWearsPrada\KillerWearsPrada\Images\textures\blueTartan.jpg", "kind" ));
+                            s.Add(new Item(2, "bc", "name2", 9.04, "descr", "rep", "texture", @"C:\Users\Monica\Documents\polimi\AUI\project\repogithub\KillerWearsPrada\KillerWearsPrada\Images\hat_1meleOk.png", "kind"));
+                            s.Add(new Item(2, "bc", "name3", 9.04, "descr", "rep", "texture", @"C:\Users\Monica\Documents\polimi\AUI\project\repogithub\KillerWearsPrada\KillerWearsPrada\Images\trashEmpty.png", "kind"));
+                        */
+            /*      items.Add(new ItemProva() { code = 345, description = "eee333", itemName = "ciaone", price = 3.5 });
+                  items.Add(new ItemProva() { description = "Jane Doe", code = 39, imageFileName = "jane@doe-family.com", price = 0.888 });
+                  items.Add(new ItemProva() { maskFileName = "Sammy Doe", price = 13, reparto = "sammy.doe@gmail.com", code = 1234, description = "capo brutto brutto" });
+                  */
             //Bindo nome listView.ItemsSource = Lista tipizzata!!!
-            lvDataBinding.ItemsSource = s;
+            //    lvDataBinding.ItemsSource = s;
+
+           // Come dovrei  per prendere gli item nell'inventario
+      //      ListView1.ItemsSource = MainWindow.attGameController.ItemsInInventory;
 
 
-            ListView1.ItemsSource = s;
+            lbOne.PreviewMouseLeftButtonDown += new System.Windows.Input.MouseButtonEventHandler(ListBox_PreviewMouseLeftButtonDown);
 
+            foreach (TimeZoneInfo tzi in TimeZoneInfo.GetSystemTimeZones())
+            {
+                if (zoneList.Count < 6)
+                    zoneList.Add(tzi);
+            }
+            lbOne.ItemsSource = zoneList;
 
 
         }
 
         private void OnClosedStoryboardCompleted(object sender, System.EventArgs e)
         {
-            
+
 
             Canvas p = (Canvas)this.Parent;
 
             enable_Right_Buttons(ref p);
 
             p.Children.Remove(this);
-       
+
         }
 
         /// <summary>
@@ -116,5 +136,135 @@ namespace KillerWearsPrada.UC
         {
 
         }
+
+
+        /// <summary>
+        /// quindi lo fa?
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void check_Checked(object sender, RoutedEventArgs e)
+        {
+            p.RemoveAt(0);
+        }
+
+        private void ListBoxItem_Selected(object sender, RoutedEventArgs e)
+        {
+            p.Remove((ItemProva)listMyItems.SelectedItem);
+        }
+
+        private void listMyItems_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SelectedItems.Add((ItemProva)listMyItems.SelectedItem);
+        }
+
+        //TODO
+        //cose per implementare drag drop listboxes
+        ObservableCollection<TimeZoneInfo> zoneList = new ObservableCollection<TimeZoneInfo>();
+        ObservableCollection<string> zoneListDest = new ObservableCollection<string>();
+
+        ListBox dragSource = null;
+        ListBox dragDestination = null;
+
+        public bool IsPressable
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool IsManipulatable
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+
+        
+        private void ListBox_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            ListBox parent = (ListBox)sender;
+            dragSource = parent;
+            object data = GetDataFromListBox(dragSource, e.GetPosition(parent));
+
+            if (data != null)
+            {
+                DragDrop.DoDragDrop(parent, data, DragDropEffects.Move);
+            }
+        }
+
+        #region GetDataFromListBox(ListBox,Point)
+        private static object GetDataFromListBox(ListBox source, Point point)
+        {
+            UIElement element = source.InputHitTest(point) as UIElement;
+            if (element != null)
+            {
+                object data = DependencyProperty.UnsetValue;
+                while (data == DependencyProperty.UnsetValue)
+                {
+                    data = source.ItemContainerGenerator.ItemFromContainer(element);
+                    if (data == DependencyProperty.UnsetValue)
+                    {
+                        element = VisualTreeHelper.GetParent(element) as UIElement;
+                    }
+                    if (element == source)
+                    {
+                        return null;
+                    }
+                }
+                if (data != DependencyProperty.UnsetValue)
+                {
+                    return data;
+                }
+            }
+            return null;
+        }
+
+        #endregion
+
+        private void ListBox_Drop(object sender, DragEventArgs e)
+        {
+            ListBox parent = (ListBox)sender;
+            object data = e.Data.GetData(typeof(TimeZoneInfo));
+            ((IList)dragSource.ItemsSource).Remove(data);
+            parent.Items.Add(data.ToString());
+        }
+
+        private void Trash_Drop(object sender, DragEventArgs e)
+        {
+            //   ListBox parent = (ListBox)sender;
+            object data = e.Data.GetData(typeof(TimeZoneInfo));
+            ((IList)dragSource.ItemsSource).Remove(data);
+            //    parent.Items.Add(data.ToString());
+        }
+
+        public IKinectController CreateController(IInputModel inputModel, KinectRegion kinectRegion)
+        {
+            throw new NotImplementedException();
+        }
     }
+
+
+
+
+    public class ItemProva
+    {
+        public ItemProva()
+        {
+
+        }
+
+        public string NomeItem { get; set; }
+        public bool isDressed
+        {
+            get; set;
+        }
+
+
+    }
+
+
 }
